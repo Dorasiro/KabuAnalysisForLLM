3efab3be9345453e62a625c35ea65e23016e708a
Dorasiro <dorasiro1@live.jp>
2025-12-12 17:07:45 +0900

    get_current_priceの動作確認完了 旧Toolsから削除

--- kabu.py
+++ kabu.py
@@ -390,52 +390,6 @@ class Tools:
 
 	def __init__(self):
 		pass
-
-	# リアルタイムの情報を返すときに使う想定　DBに格納しない
-	# ex)今～の株価何円？ -> この関数を経由して返す
-	def get_current_price(self, input: GetCurrentPriceInput) -> str:
-		try:
-			if isinstance(input, dict):
-				input = GetCurrentPriceInput(**input)
-		except ValidationError as e:
-			self.log.append_to_log_file_from_dict(input, f"ValidationError: {e}")
-			raise ValueError("入力値の形式が不正です") from e
-
-		if not input.ticker:
-			err = "銘柄コードが指定されていません"
-			self.log.append_to_log_file_from_bm(input, err)
-			raise ValueError(err)
-		
-		if not self.db.is_ticker_exists(input.ticker):
-			err = "無効な銘柄コードが指定されました"
-			self.log.append_to_log_file_from_bm(input, err)
-			raise ValueError(err)
-
-		stock = yf.Ticker(input.ticker)
-		info = stock.info
-		price = stock.history(period="1d")
-
-		# ちゃんと取得できたかを確認
-		if len(price) <= 0:
-			err = "データの取得に失敗しました"
-			self.log.append_to_log_file_from_bm(input, err)
-			raise ValueError(err)
-
-		current = info.get("currentPrice")
-
-		result = dict(
-			current = float(current) if current is not None else None,
-			open = float(price["Open"].iloc[-1]),
-			high = float(price["High"].iloc[-1]),
-			low = float(price["Low"].iloc[-1]),
-			close = float(price["Close"].iloc[-1]),
-			volume = float(price["Volume"].iloc[-1])
-		)
-
-		self.log.append_to_log_file_from_bm(input)
-		self.log.append_to_log_file_from_dict(result)
-
-		return json.dumps(result, ensure_ascii=False)
 	
 	# 指定範囲の株価情報をDBから読みだす　DBになければyfから取得する
 	# 内部用　LLMに公開する際にはDataFrameをJSONに変換する関数を挟む
