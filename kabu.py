c158880727eabd0792d1e0ceff91e7c6b39675eb
Dorasiro <dorasiro1@live.jp>
2025-12-12 19:59:31 +0900

    Open Web UIのツールとして登録するフロントエンドと実際の処理を行うバックエンドの分離作業 get_priceを移動

--- kabu.py
+++ kabu.py
@@ -326,10 +326,10 @@ class GetCurrentPriceInput(BaseModel):
     ticker: str
 
 class GetPriceInput(BaseModel):
-	ticker: str = Field(..., description="証券コード+市場サフィックス（トヨタの場合は7203.Tなど）")
-	begin_range: datetime = Field(..., description="分析開始日")
-	end_range: datetime = Field(..., description="分析終了日")
-	chart_granularity: ChartGranularity = Field(..., description="チャートの粒度（日足を使用してください）")
+	ticker: str 
+	begin_range: datetime
+	end_range: datetime
+	chart_granularity: ChartGranularity
 
 class Backend:
 	db = DB()
@@ -383,17 +383,10 @@ class Backend:
 		self.log.append_to_log_file_from_dict(result)
 
 		return json.dumps(result, ensure_ascii=False)
-
-class Tools:
-	db = DB()
-	log = Logging()
-
-	def __init__(self):
-		pass
 	
 	# 指定範囲の株価情報をDBから読みだす　DBになければyfから取得する
 	# 内部用　LLMに公開する際にはDataFrameをJSONに変換する関数を挟む
-	def _get_price(self, input: GetPriceInput) -> pandas.DataFrame:
+	def get_price(self, input: GetPriceInput) -> pandas.DataFrame:
 		try:
 			if isinstance(input, dict):
 				input = GetPriceInput(**input)
@@ -507,10 +500,130 @@ class Tools:
 
 		self.log.append_to_log_file_from_df(df)
 		return df
+
+class Tools:
+	db = DB()
+	log = Logging()
+
+	def __init__(self):
+		pass
 	
 	# 指定範囲の株価情報をDBから読みだす　DBになければyfから取得する
-	def get_price(self, input: GetPriceInput) -> str:
-		return self._get_price(input).to_json(orient="records", date_format="iso")
+	# 内部用　LLMに公開する際にはDataFrameをJSONに変換する関数を挟む
+	def get_price(self, input: GetPriceInput) -> pandas.DataFrame:
+		try:
+			if isinstance(input, dict):
+				input = GetPriceInput(**input)
+		except ValidationError as e:
+			self.log.append_to_log_file_from_dict(input, f"ValidationError: {e}")
+			raise ValueError("入力値の形式が不正です") from e
+
+		if not input.ticker:
+			err = "銘柄コードが指定されていません。"
+			self.log.append_to_log_file_from_bm(input, err)
+			raise ValueError(err)
+		
+		if not self.db.is_ticker_exists(input.ticker):
+			err = "無効な銘柄コードが指定されました。"
+			self.log.append_to_log_file_from_bm(input, err)
+			raise ValueError(err)
+		
+		if input.begin_range is None:
+			err = "解析期間の開始日が指定されていません。"
+			self.log.append_to_log_file_from_bm(input, err)
+			raise ValueError(err)
+		
+		if input.end_range is None:
+			err = "解析期間の終了日が指定されていません。"
+			self.log.append_to_log_file_from_bm(input, err)
+			raise ValueError(err)
+		
+		# これより後は両変数ともにdatetimeとして扱う
+		input.begin_range = convert_to_datetime(input.begin_range)
+		input.end_range = convert_to_datetime(input.end_range)
+		
+		# input.end_rangeが未来に設定されている場合は今日までとする
+		now = datetime.now()
+		if input.end_range > now:
+			input.end_range = now
+		
+		# 開始日と終了日が逆転してた場合のエラー
+		if input.begin_range > input.end_range:
+			err = "解析機関の開始日と終了日に矛盾があります。"
+			self.log.append_to_log_file_from_bm(input, err)
+			raise ValueError(err)
+		
+		# 指定した日が土日祝の場合はデータが取れないので範囲を狭める方向にずらす
+		# input.begin_rangeの場合は日を進める
+		while input.begin_range.weekday() >= 5 or jpholiday.is_holiday(input.begin_range):
+			input.begin_range = input.begin_range + timedelta(days=1)
+		# input.end_rangeの場合は日を戻す
+		while input.end_range.weekday() >= 5 or jpholiday.is_holiday(input.end_range):
+			input.end_range = input.end_range - timedelta(days=1)
+		
+		# 当面は日足のみ対応なのでchart_granularityに何が入力されていても日足を指定したものとして扱う
+		chart_granularity = ChartGranularity.DAILY
+		
+		# 入力に問題はなさそうなので一旦ログに書き込む
+		self.log.append_to_log_file_from_bm(input)
+
+		db = DB()
+
+		# DBに格納されているデータの範囲を取得する これらは(date, time)のタプルなので注意
+		# 当面は日足のみ対応なのでdateのみを抜き出す（Noneのときはそもそも抜き出せないから何もしない）
+		first_record: date = db.get_first_record_datetime(input.ticker)
+		if first_record is not None:
+			first_record = first_record[0]
+		end_record: date = db.get_end_record_datetime(input.ticker)
+		if end_record is not None:
+			end_record = end_record[0]
+
+		# 該当銘柄のデータが存在しない場合はinput.begin_rangeから今日までのデータを取ってくる
+		# ただし、input.begin_rangeから今日までの期間が半年未満の場合は半年分のデータを取る
+		if first_record == None and end_record == None:
+			history: pandas.DataFrame = None
+			# 概ね半年なので180日としておく
+			if now - input.begin_range >= timedelta(days=180):
+				# 半年以上の場合はinput.begin_rangeから今日までのデータを取得
+				history = yf.Ticker(input.ticker).history(start=date_to_yf_history(input.begin_range), end=date_to_yf_history(now.date()))
+			else:
+				# 半年未満の場合は今日から半年前までのデータを取得
+				history = yf.Ticker(input.ticker).history(period="6mo")
+			
+			db.insert_into_prices(input.ticker, history, chart_granularity)
+		
+		# DBに存在するデータよりも前のデータが必要な場合
+		# input.begin_rangeからfirst_recordの前日までのデータを取ってくる
+		if first_record is not None and first_record > input.begin_range.date():
+			history = yf.Ticker(input.ticker).history(start=date_to_yf_history(input.begin_range), end=date_to_yf_history((first_record - timedelta(days=1))))
+			db.insert_into_prices(input.ticker, history, chart_granularity)
+
+		# DBに存在するデータよりも後のデータが必要な場合
+		# end_recordの翌日から今日までのデータを取ってくる
+		if end_record is not None and end_record < input.end_range.date():
+			# 不足分のデータが今日のみの場合
+			if end_record + timedelta(days=1) == now.date():
+				# 現在時刻が取引時間開始時刻以降の場合はデータを取ってくる
+				if db.is_market_open(input.ticker):
+					history = yf.Ticker(input.ticker).history(period="1d")
+					db.insert_into_prices(input.ticker, history, chart_granularity)
+			# 不足分のデータが今日のみでない場合は今日のデータも存在しないので取ってくる
+			else:
+				history = yf.Ticker(input.ticker).history(start=date_to_yf_history((end_record + timedelta(days=1))), end=date_to_yf_history(now.date()))
+				db.insert_into_prices(input.ticker, history, chart_granularity)
+		
+		df = db.select_from_prices(input.ticker, input.begin_range, input.end_range, chart_granularity)
+		# データが取れていることを確認する（が、通常は問題ないはず）
+		if df is None:
+			raise LookupError("データフレームが空です。")
+		
+		# 日付をindexからレコード内に含めるように変更
+		df = df.reset_index()
+		# 日付のレコードが自動的にindexとなるのでDateに直しておく
+		df = df.rename(columns={"index": "Date",})
+
+		self.log.append_to_log_file_from_df(df)
+		return df
 	
 	# テクニカル分析の内部関数　引数、戻り値ともに他の関数と連携しやすいDataFrameとする
 	def _do_technical_analysis(self, df: pandas.DataFrame) -> pandas.DataFrame:
