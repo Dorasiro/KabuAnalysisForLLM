3f581850f475ca06e951fb50f304ee1efe039e2c
Dorasiro <dorasiro1@live.jp>
2025-12-15 02:25:26 +0900

    DBクラスを別ファイルに抽出

---
+++ DB.py
@@ -0,0 +1,230 @@
+import datetime as dt
+import MySQLdb
+import pandas
+
+class DB:
+    # 初回get_cursor実行時に接続される
+    conn: MySQLdb.Connection = None
+
+    def __init__(self):
+        pass
+
+    # DB接続
+    # タイムアウトの場合はOperationalErrorで落ちる
+    @staticmethod
+    def get_connection() ->  MySQLdb.Connection:
+        return MySQLdb.connect(
+            #host="db",        # docker-compose の service 名
+            host="192.168.2.199",
+            user="user",      # docker-compose.yml の MYSQL_USER
+            passwd="pass",    # MYSQL_PASSWORD
+            db="stocks",        # MYSQL_DATABASE
+            port=3306,
+            connect_timeout=10
+        )
+    
+    # connからcursorを取得
+    # 接続が切れていた場合は再接続する
+    @classmethod
+    def get_cursor(cls):
+        try:
+            # 初回はそのまま接続
+            if cls.conn is None:
+                cls.conn = cls.get_connection()
+            else:
+                cls.conn.ping(reconnect=True)
+        except Exception:
+            # 再接続
+            cls.conn = cls.get_connection()
+        
+        return cls.conn.cursor()
+
+    # 現在時刻が取引時間中であるか
+    def is_market_active(self, ticker: str) -> bool:
+        cur = DB.get_cursor()
+        cur.execute("""
+            SELECT 
+                m.timezone,
+                m.open1,
+                m.close1,
+                m.open2,
+                m.close2
+            FROM securities s
+            JOIN markets m ON s.market_id = m.id
+            WHERE s.code = %s
+        """, (ticker,))
+
+        row = cur.fetchone()
+        if not row:
+            return False  # 該当銘柄なし
+    
+        # 現在時刻を市場のタイムゾーンで取得
+        t = dt.datetime.now(dt.ZoneInfo(row[0])).time()
+        now = dt.timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
+    
+        # 時間1の判定
+        if row[1] <= now <= row[2]:
+            return True
+    
+        # 時間2がある場合のみ判定
+        if row[3] and row[4]:
+            if row[3] <= now <= row[4]:
+                return True
+    
+        return False
+    
+    # 現在時刻が取引開始時刻よりも後であるか
+    def is_market_open(self, ticker: str) -> bool:
+        cur = DB.get_cursor()
+        cur.execute("""
+            SELECT 
+                m.timezone,
+                m.open1
+            FROM securities s
+            JOIN markets m ON s.market_id = m.id
+            WHERE s.code = %s
+        """, (ticker,))
+
+        row = cur.fetchone()
+        if not row:
+            return False  # 該当銘柄なし
+    
+        # 現在時刻を市場のタイムゾーンで取得
+        t = dt.datetime.now(dt.ZoneInfo(row[0])).time()
+        now = dt.timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
+    
+        # 時間1の判定
+        if row[1] <= now:
+            return True
+    
+        return False
+    
+    # yfinanceから取得した株価をDBに格納する
+    # return：格納件数
+    def insert_into_prices(self, ticker: str, prices: pandas.DataFrame, chart_granularity: str) -> int:
+        if not ticker:
+            raise ValueError("ticker is empty or none")
+        
+        if prices is None or prices.empty:
+            raise ValueError("prices is empty or none")
+        
+        count = 0
+        for index, row in prices.iterrows():
+
+            time = None
+            if chart_granularity == "minute":
+                time = index.time()
+
+            cur = DB.get_cursor()
+            if index.date() == dt.date.today() and self.is_market_open(ticker):
+                cur.execute("""
+                    INSERT INTO prices (security_id, date, time, open, high, low, close, volume)
+                    VALUES ((SELECT id FROM securities WHERE code=%s), %s, %s, %s, %s, %s, %s, %s)
+                    ON DUPLICATE KEY UPDATE open = VALUES(open), high = VALUES(high), low  = VALUES(low), close = VALUES(close), volume = VALUES(volume)
+                """, (ticker, index.date(), time, row["Open"], row["High"], row["Low"], row["Close"], row["Volume"]))
+
+            else:
+                cur.execute("""
+                    INSERT INTO prices (security_id, date, time, open, high, low, close, volume)
+                    SELECT s.id, %s, %s, %s, %s, %s, %s, %s
+                    FROM securities s
+                    WHERE s.code = %s
+                """, (index.date(), time, row["Open"], row["High"], row["Low"], row["Close"], row["Volume"], ticker))
+
+            count += cur.rowcount
+
+        # トランザクション処理を確定
+        DB.conn.commit()
+        return count
+    
+    # pricesテーブルからOHLCVの値を取得する
+    # データがDBに存在するかの確認はしないので注意
+    def select_from_prices(self, ticker: str, begin_range: dt.datetime, end_range: dt.datetime, chart_granularity: str) -> pandas.DataFrame:
+        if chart_granularity == "daily":
+            # begin_range、end_rangeにに渡されているのがdatetimeである場合はdateに変換しておく
+            if type(begin_range) is dt.datetime:
+                begin_range = begin_range.date()
+
+            if type(end_range) is dt.datetime:
+                end_range = end_range.date()
+ 
+            cur = DB.get_cursor()
+            cur.execute("""
+                SELECT date, COALESCE(time, '00:00:00') AS time, open, high, low, close, volume
+                FROM prices p
+                JOIN securities s ON p.security_id = s.id
+                WHERE s.code = %s AND p.date BETWEEN %s AND %s AND p.time IS NULL
+                ORDER BY p.date ASC
+            """, (ticker, begin_range, end_range))
+            
+        else:
+            raise NotImplementedError("日足以外は未実装")
+        
+        rows = cur.fetchall()
+        dates = [pandas.to_datetime(str(d) + " " + str(t)) for d, t, *_ in rows]
+        ohlcv = [row[2:] for row in rows]
+
+        ohlcv_column = ["Open", "High", "Low", "Close", "Volume"]
+        result_df = pandas.DataFrame(ohlcv, columns=ohlcv_column, index=pandas.DatetimeIndex(dates))
+        for col in ohlcv_column:
+            result_df[col] = pandas.to_numeric(result_df[col], errors="coerce")
+
+        return result_df
+    
+    # 該当tickerが証券テーブルに登録済みであるか確認する
+    def is_ticker_exists(self, ticker: str) -> bool:
+        if not ticker:
+            return False
+        
+        cur = DB.get_cursor()
+        cur.execute("""
+                SELECT 1
+                FROM securities
+                WHERE code = %s 
+            """, (ticker,))
+        
+        return cur.fetchone() is not None
+    
+    # 該当tickerの最初のレコードの日時を取得
+    def get_first_record_datetime(self, ticker: str) -> tuple[dt.date, dt.time] | None:
+        if not ticker:
+            raise ValueError("ticker is empty or none")
+        
+        cur = DB.get_cursor()
+        cur.execute("""
+            SELECT date, time
+            FROM prices p
+            JOIN securities s ON p.security_id = s.id
+            WHERE s.code = %s
+            ORDER BY date ASC, time ASC
+            LIMIT 1
+        """, (ticker,))
+            
+        first_record = cur.fetchone()
+        # 該当するデータがDBにない場合
+        if first_record is None:
+            return None
+        
+        return first_record
+    
+    # 該当tickerの最後のレコードの日時を取得
+    def get_end_record_datetime(self, ticker: str) -> tuple[dt.date, dt.time] | None:
+        if not ticker:
+            raise ValueError("ticker is empty or none")
+        
+        cur = DB.get_cursor()
+        cur.execute("""
+            SELECT date, time
+            FROM prices p
+            JOIN securities s ON p.security_id = s.id
+            WHERE s.code = %s
+            ORDER BY date DESC, time DESC
+            LIMIT 1
+        """, (ticker,))
+            
+        end_record = cur.fetchone()
+        # 該当するデータがDBにない場合
+        if end_record is None:
+            return None
+        
+        return end_record
--- kabu.py
+++ kabu.py
@@ -6,39 +6,14 @@ import json
 import inspect
 from pydantic import BaseModel, Field, ValidationError
 import yfinance as yf
-import MySQLdb # type: ignore
 import pandas
 import pandas_ta_classic as ta
 import jpholiday
+import DB
 
 # 自作ログの出力先
 KABU_LOG_FILE = "kabu-log.txt"
 IS_LOGGING: bool = True
-
-# DB接続
-# タイムアウトの場合はOperationalErrorで落ちる
-def get_connection() ->  MySQLdb.Connection:
-	conn = MySQLdb.connect(
-    	#host="db",        # docker-compose の service 名
-		host="192.168.2.199",
-    	user="user",      # docker-compose.yml の MYSQL_USER
-    	passwd="pass",    # MYSQL_PASSWORD
-    	db="stocks",        # MYSQL_DATABASE
-    	port=3306,
-		connect_timeout=10
-	)
-	return conn
-
-conn = get_connection()
-
-def get_cursor():
-    try:
-        conn.ping(reconnect=True)
-        return conn.cursor()
-    except:
-        # 再接続
-        conn = get_connection()
-        return conn.cursor()
 
 # yfinanceのticker.historyに設定できるYYYY-MM-DDの形に変換
 # 分足データは後から取得できないため、dateのみ考慮
@@ -59,199 +34,7 @@ def convert_to_datetime(d) -> datetime:
     
     raise ValueError("引数にはdateもしくはdatetime型の変数を渡してください。")
 
-class DB:
-	def __init__(self):
-		pass
 
-		# 現在時刻が取引時間中であるか
-	def is_market_active(self, ticker: str) -> bool:
-		cur = get_cursor()
-		cur.execute("""
-			SELECT 
-    			m.timezone,
-    			m.open1,
-    			m.close1,
-    			m.open2,
-    			m.close2
-			FROM securities s
-			JOIN markets m ON s.market_id = m.id
-			WHERE s.code = %s
-		""", (ticker,))
-
-		row = cur.fetchone()
-		if not row:
-			return False  # 該当銘柄なし
-    
-    	# 現在時刻を市場のタイムゾーンで取得
-		t = datetime.now(ZoneInfo(row[0])).time()
-		now = timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
-    
-    	# 時間1の判定
-		if row[1] <= now <= row[2]:
-			return True
-    
-    	# 時間2がある場合のみ判定
-		if row[3] and row[4]:
-			if row[3] <= now <= row[4]:
-				return True
-    
-		return False
-	
-	# 現在時刻が取引開始時刻よりも後であるか
-	def is_market_open(self, ticker: str) -> bool:
-		cur = get_cursor()
-		cur.execute("""
-			SELECT 
-    			m.timezone,
-    			m.open1
-			FROM securities s
-			JOIN markets m ON s.market_id = m.id
-			WHERE s.code = %s
-		""", (ticker,))
-
-		row = cur.fetchone()
-		if not row:
-			return False  # 該当銘柄なし
-    
-    	# 現在時刻を市場のタイムゾーンで取得
-		t = datetime.now(ZoneInfo(row[0])).time()
-		now = timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
-    
-    	# 時間1の判定
-		if row[1] <= now:
-			return True
-    
-		return False
-	
-	# yfinanceから取得した株価をDBに格納する
-	# return：格納件数
-	def insert_into_prices(self, ticker: str, prices: pandas.DataFrame, chart_granularity: str) -> int:
-		if not ticker:
-			raise ValueError("ticker is empty or none")
-		
-		if prices is None or prices.empty:
-			raise ValueError("prices is empty or none")
-		
-		count = 0
-		for index, row in prices.iterrows():
-
-			time = None
-			if chart_granularity == "minute":
-				time = index.time()
-
-			cur = get_cursor()
-			if index.date() == date.today() and self.is_market_open(ticker):
-				cur.execute("""
-    				INSERT INTO prices (security_id, date, time, open, high, low, close, volume)
-    				VALUES ((SELECT id FROM securities WHERE code=%s), %s, %s, %s, %s, %s, %s, %s)
-					ON DUPLICATE KEY UPDATE open = VALUES(open), high = VALUES(high), low  = VALUES(low), close = VALUES(close), volume = VALUES(volume)
-				""", (ticker, index.date(), time, row["Open"], row["High"], row["Low"], row["Close"], row["Volume"]))
-
-			else:
-				cur.execute("""
-					INSERT INTO prices (security_id, date, time, open, high, low, close, volume)
-					SELECT s.id, %s, %s, %s, %s, %s, %s, %s
-					FROM securities s
-					WHERE s.code = %s
-				""", (index.date(), time, row["Open"], row["High"], row["Low"], row["Close"], row["Volume"], ticker))
-
-			count += cur.rowcount
-
-		# トランザクション処理を確定
-		conn.commit()
-		return count
-	
-	# pricesテーブルからOHLCVの値を取得する
-	# データがDBに存在するかの確認はしないので注意
-	def select_from_prices(self, ticker: str, begin_range: datetime, end_range: datetime, chart_granularity: str) -> pandas.DataFrame:
-		if chart_granularity == "daily":
-			# begin_range、end_rangeにに渡されているのがdatetimeである場合はdateに変換しておく
-			if type(begin_range) is datetime:
-				begin_range = begin_range.date()
-
-			if type(end_range) is datetime:
-				end_range = end_range.date()
- 
-			cur = get_cursor()
-			cur.execute("""
-				SELECT date, COALESCE(time, '00:00:00') AS time, open, high, low, close, volume
-				FROM prices p
-				JOIN securities s ON p.security_id = s.id
-				WHERE s.code = %s AND p.date BETWEEN %s AND %s AND p.time IS NULL
-            	ORDER BY p.date ASC
-			""", (ticker, begin_range, end_range))
-			
-		else:
-			raise NotImplementedError("日足以外は未実装")
-		
-		rows = cur.fetchall()
-		dates = [pandas.to_datetime(str(d) + " " + str(t)) for d, t, *_ in rows]
-		ohlcv = [row[2:] for row in rows]
-
-		ohlcv_column = ["Open", "High", "Low", "Close", "Volume"]
-		result_df = pandas.DataFrame(ohlcv, columns=ohlcv_column, index=pandas.DatetimeIndex(dates))
-		for col in ohlcv_column:
-			result_df[col] = pandas.to_numeric(result_df[col], errors="coerce")
-
-		return result_df
-	
-	# 該当tickerが証券テーブルに登録済みであるか確認する
-	def is_ticker_exists(self, ticker: str) -> bool:
-		if not ticker:
-			return False
-		
-		cur = get_cursor()
-		cur.execute("""
-				SELECT 1
-				FROM securities
-				WHERE code = %s 
-			""", (ticker,))
-		
-		return cur.fetchone() is not None
-	
-	# 該当tickerの最初のレコードの日時を取得
-	def get_first_record_datetime(self, ticker: str) -> tuple[date, time] | None:
-		if not ticker:
-			raise ValueError("ticker is empty or none")
-		
-		cur = get_cursor()
-		cur.execute("""
-			SELECT date, time
-			FROM prices p
-			JOIN securities s ON p.security_id = s.id
-			WHERE s.code = %s
-			ORDER BY date ASC, time ASC
-			LIMIT 1
-		""", (ticker,))
-			
-		first_record = cur.fetchone()
-		# 該当するデータがDBにない場合
-		if first_record is None:
-			return None
-		
-		return first_record
-	
-	# 該当tickerの最後のレコードの日時を取得
-	def get_end_record_datetime(self, ticker: str) -> tuple[date, time] | None:
-		if not ticker:
-			raise ValueError("ticker is empty or none")
-		
-		cur = get_cursor()
-		cur.execute("""
-			SELECT date, time
-			FROM prices p
-			JOIN securities s ON p.security_id = s.id
-			WHERE s.code = %s
-			ORDER BY date DESC, time DESC
-			LIMIT 1
-		""", (ticker,))
-			
-		end_record = cur.fetchone()
-		# 該当するデータがDBにない場合
-		if end_record is None:
-			return None
-		
-		return end_record
 
 class Logging:
 		# 所定のログファイルにログを追記する
@@ -343,7 +126,7 @@ class GetPriceInput(BaseModel):
 	chart_granularity: str
 
 class Backend:
-	db = DB()
+	db = DB.DB()
 	log = Logging()
 
 	def __init__(self):
@@ -454,14 +237,12 @@ class Backend:
 		# 入力に問題はなさそうなので一旦ログに書き込む
 		self.log.append_to_log_file_from_bm(input)
 
-		db = DB()
-
 		# DBに格納されているデータの範囲を取得する これらは(date, time)のタプルなので注意
 		# 当面は日足のみ対応なのでdateのみを抜き出す（Noneのときはそもそも抜き出せないから何もしない）
-		first_record: date = db.get_first_record_datetime(input.ticker)
+		first_record: date = self.db.get_first_record_datetime(input.ticker)
 		if first_record is not None:
 			first_record = first_record[0]
-		end_record: date = db.get_end_record_datetime(input.ticker)
+		end_record: date = self.db.get_end_record_datetime(input.ticker)
 		if end_record is not None:
 			end_record = end_record[0]
 
@@ -477,13 +258,13 @@ class Backend:
 				# 半年未満の場合は今日から半年前までのデータを取得
 				history = yf.Ticker(input.ticker).history(period="6mo")
 			
-			db.insert_into_prices(input.ticker, history, chart_granularity)
+			self.db.insert_into_prices(input.ticker, history, chart_granularity)
 		
 		# DBに存在するデータよりも前のデータが必要な場合
 		# input.begin_rangeからfirst_recordの前日までのデータを取ってくる
 		if first_record is not None and first_record > input.begin_range.date():
 			history = yf.Ticker(input.ticker).history(start=date_to_yf_history(input.begin_range), end=date_to_yf_history((first_record - timedelta(days=1))))
-			db.insert_into_prices(input.ticker, history, chart_granularity)
+			self.db.insert_into_prices(input.ticker, history, chart_granularity)
 
 		# DBに存在するデータよりも後のデータが必要な場合
 		# end_recordの翌日から今日までのデータを取ってくる
@@ -491,15 +272,15 @@ class Backend:
 			# 不足分のデータが今日のみの場合
 			if end_record + timedelta(days=1) == now.date():
 				# 現在時刻が取引時間開始時刻以降の場合はデータを取ってくる
-				if db.is_market_open(input.ticker):
+				if self.db.is_market_open(input.ticker):
 					history = yf.Ticker(input.ticker).history(period="1d")
-					db.insert_into_prices(input.ticker, history, chart_granularity)
+					self.db.insert_into_prices(input.ticker, history, chart_granularity)
 			# 不足分のデータが今日のみでない場合は今日のデータも存在しないので取ってくる
 			else:
 				history = yf.Ticker(input.ticker).history(start=date_to_yf_history((end_record + timedelta(days=1))), end=date_to_yf_history(now.date()))
-				db.insert_into_prices(input.ticker, history, chart_granularity)
+				self.db.insert_into_prices(input.ticker, history, chart_granularity)
 		
-		df = db.select_from_prices(input.ticker, input.begin_range, input.end_range, chart_granularity)
+		df = self.db.select_from_prices(input.ticker, input.begin_range, input.end_range, chart_granularity)
 		# データが取れていることを確認する（が、通常は問題ないはず）
 		if df is None:
 			raise LookupError("データフレームが空です。")
